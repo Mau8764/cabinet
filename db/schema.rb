86f58087d6d4d3abfdc5aecf639cc79526b5e31c
Mauricio Vanegas <mau8764@gmail.com>
2022-01-11 20:21:47 -0600

    Association between User and Documents

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_12_020727) do
+ActiveRecord::Schema.define(version: 2022_01_12_021225) do
 
   create_table "documents", force: :cascade do |t|
     t.string "tittle"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
